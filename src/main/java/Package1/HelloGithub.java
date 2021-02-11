b8e5dd0d63f8832fec239e53031e9d5e89f9e0dc
Wall1885 <72877427+Wall1885@users.noreply.github.com>
2021-02-11 16:43:06 +0000

    Second commit 16:43

--- src/main/java/Package1/HelloGithub.java
+++ src/main/java/Package1/HelloGithub.java
@@ -5,6 +5,6 @@ public class HelloGithub {
                 public static void main(String[] args) {
 
             System.out.println("Hello Github");
-
+                    System.out.println("I have landed in Github!!!");
         }
     }
