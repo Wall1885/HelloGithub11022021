de34cd2772ef52940ada67eaf7f9c73f26583942
Wall1885 <sos69.glenera@live.co.uk>
2021-02-19 14:26:02 +0000

    upadte 19022021

---
+++ src/main/java/Package1/HelloDeploy.java
@@ -0,0 +1,11 @@
+package Package1;
+
+public class HelloDeploy {
+public static void main (String[] args) {
+
+    System.out.println("This is the Deploy and it has been successful!!!!");
+}
+}
+
+
+
